18febd8320c8f958593cad4fd00b691c6e5e7803
lfmen <lucayankeemengarelli@gmail.com>
2026-05-07 17:22:42 -0300

    Add files via upload

---
+++ jugar.R
@@ -0,0 +1,48 @@
+#-------------------------------------------------------------------------------
+#       PROGRAMACIÓN 1 - TRABAJO PRÁCTICO - AÑO 2026
+#-------------------------------------------------------------------------------
+#
+# EQUIPO Nº xx:
+#
+# - Apellido, Nombre
+# - Apellido, Nombre
+# - Apellido, Nombre
+# - Apellido, Nombre
+#
+# ARCHIVO: jugar.R
+#
+# Este archivo debe ser ejecutado desde una terminal con la instrucción Rscript
+# jugar.R, desde el directorio en el que esté guardado junto con cualquier otro
+# archivo que sea creado como parte de la solución.
+#
+# Agregar aquí cualquier comentario que consideren importante sobre la
+# resolución del TP o sobre este archivo de código.
+#-------------------------------------------------------------------------------
+
+#-------------------------------------------------------------------------------
+# Paquete "farkle"
+#-------------------------------------------------------------------------------
+
+# El paquete "farkle" ofrece funciones auxiliares que simplifican muchas partes
+# de la solución de este trabajo. Se recomienda leer la documentación con
+# atención y correr los ejemplos.
+# Las líneas de código que se encargan de la instalación del paquete no deben
+# quedar en este script, pero sí la sentencia library para cargarlo.
+
+# Instalación (correr una sola vez, luego comentar o eliminar):
+# install.packages("pak")
+# pak::pkg_install("ee-unr/programacion-1/tp/farkle")
+
+library("farkle")
+
+#-------------------------------------------------------------------------------
+# Carga de funciones propias
+#-------------------------------------------------------------------------------
+
+source("funciones.R")
+
+#-------------------------------------------------------------------------------
+# Programa principal
+#-------------------------------------------------------------------------------
+
+# COMPLETAR
